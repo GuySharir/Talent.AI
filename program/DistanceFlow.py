b3c8c711ec678804673e4854782d223f2c1aeabe
opalpeltzman <61938357+opalpeltzman@users.noreply.github.com>
2022-02-26 20:26:39 +0200

    done - save domain size and frequency to file to do - implement eq11 and eq14 and test distance calculation for 2 instances

--- program/DistanceFlow.py
+++ program/DistanceFlow.py
@@ -1,7 +1,9 @@
 import os
+import json
 import pandas as pd
 import numpy as np
 from algorithems.domain_size_frequencies import DomainAndFrequency
+from algorithems.DistanceCalc import Distance
 
 
 class DistanceFlow:
@@ -13,14 +15,18 @@ class DistanceFlow:
         calculate distance matrix for each data set in the data base
 
         - self.df, represents all employees data from json files
+        - self.domain_per_attribute, for each attribute in the data represent its domain size
+        (the number of possible values)
+        - self.freq_per_attribute, for each attributes' value in the data represent its frequency
     """
-    def __init__(self):
+    def __init__(self, calc_domain_freq=False):
         self.df = None
+        self.calc_domain_freq = calc_domain_freq
         self.domain_per_attribute = {}
         self.freq_per_attribute = {}
+        self.attr_types = {}
 
-    def calc_domain_and_frequency(self):
-        attributes = {}
+    def open_json_and_attr_types(self):
         types = [str, int, float, dict, list]
         data_path = os.path.abspath(os.path.
                                     join(os.path.dirname(__file__), '..', 'dataTool\\clean_data')).replace('/', '\\')
@@ -38,13 +44,41 @@ class DistanceFlow:
                 if data_type not in types:
                     print(f'unpredicted value type')
 
-                attributes[attr] = data_type
-            print(f'attributes dict {attributes}')
+                self.attr_types[attr] = data_type
+            print(f'attributes dict {self.attr_types}')
 
-        for attr, val_type in attributes.items():
+    def calc_domain_and_frequency(self):
+        for attr, val_type in self.attr_types.items():
             print(f'attribute {attr}')
-            DomainAndFrequency(attr=attr, val_type=val_type, data_frame=self.df).calc_domain_and_frequency()
+            value_frequency, domain_size =\
+                DomainAndFrequency(attr=attr, val_type=val_type, data_frame=self.df).calc_domain_and_frequency()
+
+            self.domain_per_attribute.update({attr: domain_size})
+            self.freq_per_attribute.update({attr: value_frequency})
+        print(f'domain per attribute {self.domain_per_attribute}')
+        print(f'freq per attribute {self.freq_per_attribute}')
+
+        domain_size_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'dataTool\\domain_size'))\
+            .replace('/', '\\')
+        domain_path = os.path.abspath(os.path.join(domain_size_path, 'attributes_domain_size.json'))
+        with open(domain_path, 'w') as fp:
+            json.dump(self.domain_per_attribute, fp)
+
+        freq_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'dataTool\\frequencies'))\
+            .replace('/', '\\')
+        frequencies_path = os.path.abspath(os.path.join(freq_path, 'attributes_frequency.json'))
+        with open(frequencies_path, 'w') as fp:
+            json.dump(self.freq_per_attribute, fp)
+
+    def run_distance_flow(self):
+        self.open_json_and_attr_types()
+        if self.calc_domain_freq:
+            print('here')
+            self.calc_domain_and_frequency()
+        # print(self.df.iloc[0][0])
+        # print(type(self.df.iloc[0]))
+        Distance(self.df.iloc[0], self.df.iloc[1], self.attr_types).calc_distance()
 
 
 if __name__ == '__main__':
-    DistanceFlow().calc_domain_and_frequency()
+    DistanceFlow(False).run_distance_flow()
