28e488db0b911d158e6889fd6b0b20975ccb5492
opalpeltzman <opalpeltzman@gmail.com>
2022-03-14 22:46:28 +0200

    functionality.py flow organization adding general str, num and list dist calculation function that can be called from nested calculation function

--- distance/DistanceFunctionality.py
+++ distance/DistanceFunctionality.py
@@ -4,7 +4,7 @@ from distance.ListsDistance import ListsDistance
 from distance.NestedDistance import NestedDistance
 from distance.DistEnum import ListDistMethod
 from distance.DistEnum import NestedDistMethod
-from distance.DistanceStrFunctions import DistanceStrFunctions
+from distance.DistanceFunctions import DistanceNumStr
 
 
 class DistanceFunctionality:
@@ -51,49 +51,35 @@ class DistanceFunctionality:
     def q11(self):
         for attr, val in self.instance_a.items():
             val_type = locate(self.attr_types[attr].split("'")[1])
-
+            dist_obj = DistanceNumStr()
             if val_type == str:
-
-                print(val_type)
-                print(f'attr {attr}')
-                print(self.domain_per_attribute[attr])
-                print(f'instance a value - {val}\ninstance b value - {self.instance_b[attr]}')
-                print(f'the attribute value freq dictionary {self.freq_per_attribute[attr]}')
-                q10result = DistanceStrFunctions().q10(val1=val, val2=self.instance_b[attr],
-                                                       value_frequency=self.freq_per_attribute[attr],
-                                                       domain_size=self.domain_per_attribute[attr])
-                q12result = DistanceStrFunctions().q12(attribute=attr, instance_a=self.instance_a,
-                                                       instance_b=self.instance_b)
-
-                print(f'q10 result {q10result}\nq12 result {q12result}')
-                self.categorical_sum += (q10result * q12result)
-                print("####################################################################################")
+                result = dist_obj.distance_per_type(val_type=val_type, val1=val, val2=self.instance_b[attr],
+                                                    value_frequency=self.freq_per_attribute[attr],
+                                                    domain_size=self.domain_per_attribute[attr],
+                                                    attribute=attr, instance_a=self.instance_a,
+                                                    instance_b=self.instance_b)
+                self.categorical_sum += result
 
             elif val_type == float or val_type == int:
-                q13result = DistanceStrFunctions().calc_num_distance_q13(val1=val, val2=self.instance_b[attr])
-
-                print(val_type)
-                print(f'attr {attr}')
-                print(f'q13 result {q13result}\n')
-                self.numerical_sum += q13result
-                print("####################################################################################")
+                result = dist_obj.distance_per_type(val_type=val_type, val1=val,
+                                                    val2=self.instance_b[attr],
+                                                    value_frequency=self.freq_per_attribute[attr],
+                                                    domain_size=self.domain_per_attribute[attr], attribute=attr)
+                self.numerical_sum += result
 
             elif val_type == list:
-                list_dist_obj = ListsDistance(freq_per_attribute=self.freq_per_attribute[attr],
-                                              list1=val, list2=self.instance_b[attr],
-                                              lists_dist_method=self.lists_dist_method)
-
-                list_dist_result = list_dist_obj.calc_dist()
-                self.categorical_sum += list_dist_result
-
-                print(val_type)
-                print(f'attr {attr}')
-                print(f'list dist result {list_dist_result}\n')
-                print("####################################################################################")
+                result = dist_obj.distance_per_type(val_type=val_type, val1=val,
+                                                    val2=self.instance_b[attr],
+                                                    value_frequency=self.freq_per_attribute[attr],
+                                                    domain_size=self.domain_per_attribute[attr], attribute=attr,
+                                                    instance_a=None, instance_b=None,
+                                                    lists_dist_method=self.lists_dist_method)
+                self.categorical_sum += result
 
             elif val_type == dict:
                 nested_dist_obj = NestedDistance(freq_per_attribute=self.freq_per_attribute[attr], attribute=attr,
-                                                 nested_obj1=val, nested_obj2=self.instance_b[attr])
+                                                 nested_obj1=val, nested_obj2=self.instance_b[attr],
+                                                 nested_dist_method=self.nested_dist_method)
                 nested_dist_obj.calc_dist()
 
         print(f'categorical sum result {self.categorical_sum}')
@@ -106,21 +92,14 @@ class DistanceFunctionality:
 
         print(f'attr types {self.attr_types}')
         print(f'nested attr types {self.nested_attr_types}')
-        # print(f'freq per attr {self.freq_per_attribute}')
-        # print(f'domain per attr {self.domain_per_attribute}')
-        #
-        # self.instance_a = {attr: self.instance_a[inx] for inx, attr in enumerate(self.attr_types.keys())}
-        # self.instance_b = {attr: self.instance_b[inx] for inx, attr in enumerate(self.attr_types.keys())}
         print(f'instance a - {self.instance_a}')
         print(f'instance b - {self.instance_b}')
 
         self.q11()
-        # print(f'total distance result {self.q14()}')
+
         return self.q14()
 
 
 if __name__ == '__main__':
-    interest = {'web applications': 2, 'web development': 4,
-                'programming': 3, 'entrepreneurship': 3}
-
+    pass
 
