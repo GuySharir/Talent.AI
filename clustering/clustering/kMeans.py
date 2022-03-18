904033a50b98e89ba5a2510da4ba69e4111c2b67
GuySharir <61948800+GuySharir@users.noreply.github.com>
2022-03-18 20:57:45 +0200

    no dp

--- clustering/clustering/kMeans.py
+++ clustering/clustering/kMeans.py
@@ -1,8 +1,18 @@
 import json
+import time
+
 import numpy as np
 import pandas as pd
 from numpy.linalg import norm
 from program.DistanceFlow import DistanceFlow
+from distance.DistEnum import ListDistMethod
+from distance.DistEnum import NestedDistMethod
+import inspect
+
+
+def my_print(message):
+    # print(inspect.stack()[1][3])
+    print(message)
 
 
 class Kmeans:
@@ -15,7 +25,9 @@ class Kmeans:
         self.load_data(dataPath)
         self.centroids = []
         self.clusters = [[] for _ in range(n_clusters)]
-        self.distance_calc: DistanceFlow = DistanceFlow(False, False)
+        self.distance_calc: DistanceFlow = DistanceFlow(False, False, ListDistMethod.intersection,
+                                                        NestedDistMethod.all_items)
+        self.dp = {}
 
     def load_data(self, dataPath: str) -> pd.DataFrame:
         with open(dataPath, 'r') as file:
@@ -23,11 +35,14 @@ class Kmeans:
             self.data = pd.DataFrame.from_records(json_data)
             self.data = self.data.sample(frac=1).reset_index(drop=True)
             self.data = self.data.sample(n=30)
+            self.data.reset_index(drop=True, inplace=True)
 
     def initialize_centroids(self):
-        for idx, group in enumerate(self.clusters):
+        idx = 0
+        for cluster in self.clusters:
             self.centroids.append(idx)
-            group.append(idx)
+            cluster.append(idx)
+            idx += 1
 
     def clear_clusters(self):
         for cluster in self.clusters:
@@ -38,64 +53,63 @@ class Kmeans:
         # in order to reduce complexity
 
         new_centroids = []
-        for idx, cluster in self.clusters:
+        idx = 0
+
+        for cluster in self.clusters:
             if len(cluster) <= 2:
                 new_centroids.append(self.centroids[idx])
             else:
-                distances = [[] for _ in len(cluster)]
-                for i, a in enumerate(cluster):
-                    for j, b in enumerate(cluster):
-                        distances[i][j] = distances[j][i] = self.distance_calc.run_distance_flow(self.data.iloc[a],
-                                                                                                 self.data.iloc[b])
+                if idx not in self.dp:
+                    distances = [[] for _ in range(len(cluster))]
+                    for i in range(len(cluster)):
+                        for j in range(len(cluster)):
+                            distances[i].append(self.distance_calc.dis_for_clustering(
+                                self.data.iloc[cluster[i]],
+                                self.data.iloc[cluster[j]]))
 
-                scores = []
-                for i in range(len(distances)):
-                    scores.append(sum(distances[i]))
+                    scores = []
+                    for i in range(len(distances)):
+                        scores.append(sum(distances[i]))
 
-                new_centroids.append(np.argmin(scores))
+                    new_centroids.append(np.argmin(scores))
+            idx += 1
 
         return new_centroids
 
     def find_closest_cluster(self, entry):
         distances = []
         for centroid in self.centroids:
-            distances.append(self.distance_calc.run_distance_flow(entry, self.data.iloc[centroid]))
+            if np.all(self.data.iloc[centroid] == entry):
+                return None
 
-        print(distances)
+            distances.append(self.distance_calc.dis_for_clustering(entry, self.data.iloc[centroid]))
+
         return np.argmin(distances)
 
     def add_to_cluster(self, centroid_idx, entry):
-        centroid = self.centroids[centroid_idx]
 
-        res_cluster = None
         for cluster in self.clusters:
-            if centroid in cluster:
-                res_cluster = cluster
+            if centroid_idx in cluster:
+                cluster.append(entry)
                 break
 
-        res_cluster.add(entry)
-
-    # def compute_sse(self, X, labels, centroids):
-    #     distance = np.zeros(X.shape[0])
-    #     for k in range(self.n_clusters):
-    #         distance[labels == k] = norm(X[labels == k] - centroids[k], axis=1)
-    #     return np.sum(np.square(distance))
+        print(self.clusters)
 
     def fit(self):
         self.models = []
         self.initialize_centroids()
         for i in range(self.max_iter):
             # self.initialize_centroids()
-            self.clear_clusters()
 
-            for idx, entry in enumerate(self.data.iterrows()):
+            for idx, entry in self.data.iterrows():
                 centroid_idx = self.find_closest_cluster(entry)
-                self.add_to_cluster(centroid_idx, idx)
-                new_centroids = self.calc_centroids()
+                if centroid_idx is not None:
+                    self.add_to_cluster(centroid_idx, idx)
+                    new_centroids = self.calc_centroids()
 
-                # self.centroids = self.compute_centroids(X, self.labels)
-                if np.all(new_centroids == self.centroids):
-                    break
+                    # self.centroids = self.compute_centroids(X, self.labels)
+                    # if np.all(new_centroids == self.centroids):
+                    #     break
 
             self.models.append((self.centroids, self.clusters))
 
@@ -108,4 +122,4 @@ x = Kmeans('../../dataTool/clean_data/dataSet/test.json', 4, 10)
 x.fit()
 
 for model in x.models:
-    print(model)
+    my_print(model)
