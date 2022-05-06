4be26869936eaa8ad61faa690bf514326f08f913
opalpeltzman <opalpeltzman@gmail.com>
2022-05-06 21:21:40 +0300

    finished hamming representation and hamming distance calculation 2. distance for hamming(when $ don't include in distance calculation) -  done V TO DO - 1. apply representation for one hot including extra space for future changes. 2. distance for inner, intersection options

--- program/DistanceFlow.py
+++ program/DistanceFlow.py
@@ -1,9 +1,10 @@
 from ReadData import read_freq_per_value_data, read_domain_per_attr_data, DATA_TYPE_PER_INDEX, \
                      ATTRIBUTE_PER_INDEX, DATA_TYPE_PER_INDEX_EXPERIENCE, DATA_TYPE_PER_INDEX_EDUCATION, EXPERIENCE_OBJECT_ATTR_LENGTH,\
-                     EDUCATION_OBJECT_ATTR_LENGTH, EXPERIENCE_ATTRIBUTE_PER_INDEX, EDUCATION_ATTRIBUTE_PER_INDEX
+                     EDUCATION_OBJECT_ATTR_LENGTH, EXPERIENCE_ATTRIBUTE_PER_INDEX, EDUCATION_ATTRIBUTE_PER_INDEX, HAMMING_DEFAULT
 from dataTool.runtimeObjectsInfo.ListLengthData import LIST_LENGTH_PER_ATTR, NESTED_LENGTH_PER_ATTR, EXPERIENCE_LISTS_LENGTH, EDUCATION_LISTS_LENGTH
 from DistEnum import DistMethod
 from DistFunctions import DistanceData, q14, categorical_dist_between_freq_vectors, numerical_dist_between_freq_vectors
+import numpy as np
 
 
 def logger(*args):
@@ -21,13 +22,15 @@ class DistanceFlowFreq:
 
         self.cat_distance_result = []
         self.num_distance_result = []
-        self.freq_distance_result = None
 
     def numerical_distance(self, representation_option: DistMethod, attr_type, num1, num2):
         if num1 and num2:   # dealing with missing values when numerical type by deleting missing data
             if representation_option == DistMethod.fix_length_freq:
                 self.num_distance_result.append(numerical_dist_between_freq_vectors(attr_type=attr_type,
                                                                                     num_val1=num1, num_val2=num2))
+            elif representation_option == DistMethod.hamming_distance:
+                if self.vec1[self.inx] != HAMMING_DEFAULT and self.vec2[self.inx] != HAMMING_DEFAULT:
+                    self.num_distance_result.append(np.sqrt((num1 - num2) ** 2))
 
     def categorical_distance(self, representation_option: DistMethod, attr_type, frequency: dict, domain_size: int):
 
@@ -40,6 +43,13 @@ class DistanceFlowFreq:
             self.cat_distance_result.append(result)
             # self.cat_distance_result.append(categorical_dist_between_freq_vectors(attr_type=attr_type, data=data))
 
+        elif representation_option == DistMethod.hamming_distance:
+            if self.vec1[self.inx] != HAMMING_DEFAULT and self.vec2[self.inx] != HAMMING_DEFAULT:
+                if self.vec1[self.inx] == self.vec2[self.inx]:
+                    self.cat_distance_result.append(0)
+                else:
+                    self.cat_distance_result.append(1)
+
     def set_distance(self, attr_name: str, frequency: dict, domain_size: int):
         # each set contains only categorical values in different representations
         attr_length = LIST_LENGTH_PER_ATTR[attr_name]
@@ -49,7 +59,7 @@ class DistanceFlowFreq:
         logger(f'set1-\n{set1}')
         logger(f'set2-\n{set2}')
 
-        if self.representation_option_set == DistMethod.fix_length_freq:
+        if self.representation_option_set == DistMethod.fix_length_freq or self.representation_option_set == DistMethod.hamming_distance:
             for _ in range(attr_length):
                 self.categorical_distance(representation_option=self.representation_option_set, attr_type=str, frequency=frequency, domain_size=domain_size)
                 self.inx += 1
@@ -118,10 +128,15 @@ class DistanceFlowFreq:
         print(f'categorical result-\n {self.cat_distance_result}')
         print(f'categorical list length-\n {len(self.cat_distance_result)}')
         print(f'numerical result-\n {self.num_distance_result}')
-        self.freq_distance_result = q14(categorical_sum=sum(self.cat_distance_result), numerical_sum=sum(self.num_distance_result))
-        print(f'distance result-\n {self.freq_distance_result}')
 
-        return self.freq_distance_result
+        if self.representation_option == DistMethod.hamming_distance:
+            hamming_result = np.sqrt((sum(self.cat_distance_result) - sum(self.num_distance_result)) ** 2)
+            print(f'hamming distance result-\n {hamming_result}')
+            return hamming_result
+        else:
+            freq_distance_result = q14(categorical_sum=sum(self.cat_distance_result), numerical_sum=sum(self.num_distance_result))
+            print(f'freq distance result-\n {freq_distance_result}')
+            return freq_distance_result
 
 
 def run_distance_freq(vec1: list, vec2: list, representation_option: DistMethod, representation_option_set: DistMethod) -> float:
@@ -129,9 +144,12 @@ def run_distance_freq(vec1: list, vec2: list, representation_option: DistMethod,
 
 
 if __name__ == '__main__':
-    instance1_ = {'laura gao': ('twitter', [1, 1, 1, 5, 1996.0, 57, 14, 1, 7, 7, 1, 61, 61, 4, 1, 1, 1, 1, 14, 10, 7, 1, 1, 1, 68, 1, 2013, 5, 4, 1, 709, 1, 650, 658, 1, 44, 117, 2, 233, 2, 1994, 165, 5, 8, 709, 11, 650, 658, 1, 491, 117, 1, 170, 1, 2010, 2, 14, 14, 709, 4, 650, 658, 1, 179, 117, 91, 170, 91, 2006, 165, 92, 7, 98, 156, 650, 658, 1, 44, 1, 1, 1, 1, None, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 35, 58, 100, None, 104, 1, 1, 2, 42, 1, 100, None, 1, 1, 1, 1, 225, 8, 11, 3.64, 104, 2, 1])}
-    instance2_ = {'bruk argaw': ('twitter', [1, 1, 1, 93, 1991.0, 1, 14, 18, 88, 82, 1, 61, 61, 1, 34, 28, 22, 10, 14, 11, 10, 6, 1, 1, 26, 1, 1916, 76, 6, 4, 709, 25, 650, 658, 1, 52, 1, 5, 233, 5, 1868, 76, 14, 4, 709, 5, 650, 658, 1, 52, 1, 5, 233, 5, 1868, 76, 5, 11, 709, 5, 650, 658, 1, 52, 1, 91, 170, 91, 2006, 165, 92, 1, 98, 156, 650, 658, 72, 491, 1, 1, 82, 1, 2016, 3, 3, 6, 709, 156, 650, 658, 72, 491, 1, 1, 225, 58, 100, None, 1, 1, 1, 1, 225, 58, 3, None, 1, 7, 1, 1, 42, 21, 100, None, 1, 1, 1])}
+    # # freq representation
+    # vec1_ = [1, 1, 1, 5, 1996.0, 57, 14, 1, 7, 7, 1, 61, 61, 4, 1, 1, 1, 1, 14, 10, 7, 1, 1, 1, 68, 1, 2013, 5, 4, 1, 709, 1, 650, 658, 1, 44, 117, 2, 233, 2, 1994, 165, 5, 8, 709, 11, 650, 658, 1, 491, 117, 1, 170, 1, 2010, 2, 14, 14, 709, 4, 650, 658, 1, 179, 117, 91, 170, 91, 2006, 165, 92, 7, 98, 156, 650, 658, 1, 44, 1, 1, 1, 1, None, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 35, 58, 100, None, 104, 1, 1, 2, 42, 1, 100, None, 1, 1, 1, 1, 225, 8, 11, 3.64, 104, 2, 1]
+    # vec2_ = [1, 1, 1, 93, 1991.0, 1, 14, 18, 88, 82, 1, 61, 61, 1, 34, 28, 22, 10, 14, 11, 10, 6, 1, 1, 26, 1, 1916, 76, 6, 4, 709, 25, 650, 658, 1, 52, 1, 5, 233, 5, 1868, 76, 14, 4, 709, 5, 650, 658, 1, 52, 1, 5, 233, 5, 1868, 76, 5, 11, 709, 5, 650, 658, 1, 52, 1, 91, 170, 91, 2006, 165, 92, 1, 98, 156, 650, 658, 72, 491, 1, 1, 82, 1, 2016, 3, 3, 6, 709, 156, 650, 658, 72, 491, 1, 1, 225, 58, 100, None, 1, 1, 1, 1, 225, 58, 3, None, 1, 7, 1, 1, 42, 21, 100, None, 1, 1, 1]
+    # run_distance_freq(vec1=vec1_, vec2=vec2_, representation_option=DistMethod.fix_length_freq, representation_option_set=DistMethod.fix_length_freq)
 
-    vec1_ = [1, 1, 1, 5, 1996.0, 57, 14, 1, 7, 7, 1, 61, 61, 4, 1, 1, 1, 1, 14, 10, 7, 1, 1, 1, 68, 1, 2013, 5, 4, 1, 709, 1, 650, 658, 1, 44, 117, 2, 233, 2, 1994, 165, 5, 8, 709, 11, 650, 658, 1, 491, 117, 1, 170, 1, 2010, 2, 14, 14, 709, 4, 650, 658, 1, 179, 117, 91, 170, 91, 2006, 165, 92, 7, 98, 156, 650, 658, 1, 44, 1, 1, 1, 1, None, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 35, 58, 100, None, 104, 1, 1, 2, 42, 1, 100, None, 1, 1, 1, 1, 225, 8, 11, 3.64, 104, 2, 1]
-    vec2_ = [1, 1, 1, 93, 1991.0, 1, 14, 18, 88, 82, 1, 61, 61, 1, 34, 28, 22, 10, 14, 11, 10, 6, 1, 1, 26, 1, 1916, 76, 6, 4, 709, 25, 650, 658, 1, 52, 1, 5, 233, 5, 1868, 76, 14, 4, 709, 5, 650, 658, 1, 52, 1, 5, 233, 5, 1868, 76, 5, 11, 709, 5, 650, 658, 1, 52, 1, 91, 170, 91, 2006, 165, 92, 1, 98, 156, 650, 658, 72, 491, 1, 1, 82, 1, 2016, 3, 3, 6, 709, 156, 650, 658, 72, 491, 1, 1, 225, 58, 100, None, 1, 1, 1, 1, 225, 58, 3, None, 1, 7, 1, 1, 42, 21, 100, None, 1, 1, 1]
-    run_distance_freq(vec1=vec1_, vec2=vec2_, representation_option=DistMethod.fix_length_freq, representation_option_set=DistMethod.fix_length_freq)
+    # hamming representation
+    vec1_ = ['laura gao', 'laura', 'gao', 'female', 1996.0, None, 'internet', 'associate product manager ii', 'operations', 'product', '$', 'twitter', 'twitter', '2018-09', 'potenciamiento económico', '$', '$', '$', 'public speaking', 'management', 'microsoft office', 'technology', 'creative strategy', 'smartypal', '1-10', 'smartypal', 2013, 'e-learning', '2017', '2017', False, 'philadelphia, pennsylvania, united states', 'united states', 'north america', 'product management intern', 'operations', 'training', 'amazon', '10001+', 'amazon', 1994, 'internet', '2017-08', '2017-06', False, 'seattle, washington, united states', 'united states', 'north america', 'business data analyst intern', 'engineering', 'training', 'consumer financial protection bureau', '1001-5000', 'consumer-financial-protection-bureau', 2010, 'government administration', '2015-08', '2015-06', False, 'washington, district of columbia, united states', 'united states', 'north america', 'chief of staff policy intern', None, 'training', 'twitter', '1001-5000', 'twitter', 2006, 'internet', None, '2018-09', True, 'san francisco, california, united states', 'united states', 'north america', 'associate product manager ii', 'operations', '$', '$', '$', '$', '$', '$', '$', '$', '$', '$', '$', '$', '$', '$', '$', 'graduate school of city planning minor', None, None, None, None, 'bachelors', '$', '$', 'coppell high school', 'secondary school', '2014-05', None, None, '$', '$', '$', 'the wharton school', 'post-secondary institution', '2018', '2014', 3.64, 'bachelors', 'economics', '$']
+    vec2_ = ['bruk argaw', 'bruk', 'argaw', 'male', 1991.0, '1991-01-23', 'internet', 'software engineer', 'engineering', 'software', '$', 'twitter', 'twitter', '2020-03', 'environment', 'education', 'poverty alleviation', 'science and technology', 'matlab', 'engineering', 'microsoft excel', 'microsoft word', 'research', 'sacramento city college', '501-1000', 'sacramento-city-college', 1916, 'higher education', '2013-04', '2013-01', False, 'united states', 'united states', 'north america', 'lab ta', 'education', '$', 'uc berkeley', '10001+', 'uc-berkeley', 1868, 'higher education', '2015-08', '2015-05', False, 'berkeley, california, united states', 'united states', 'north america', 'research assistant @ pavement research center', 'education', '$', 'uc berkeley', '10001+', 'uc-berkeley', 1868, 'higher education', '2014-04', '2014-01', False, 'berkeley, california, united states', 'united states', 'north america', 'research assistant @ davis labs', 'education', '$', 'twitter', '1001-5000', 'twitter', 2006, 'internet', None, '2020-03', True, 'san francisco, california, united states', 'united states', 'north america', 'software engineer', 'engineering', '$', 'indio technologies inc.', '51-200', 'indiotechnologies', 2016, 'insurance', '2019-05', '2017-08', False, 'san francisco, california, united states', 'united states', 'north america', 'software engineer', 'engineering', '$', 'sacramento city college', 'post-secondary institution', None, None, None, '$', '$', '$', 'app academy', 'post-secondary institution', None, '2017', None, '$', 'software engineering', '$', 'el camino fundamental high school', 'secondary school', '2010', None, None, '$', '$', '$']
+    run_distance_freq(vec1=vec1_, vec2=vec2_, representation_option=DistMethod.hamming_distance, representation_option_set=DistMethod.hamming_distance)
